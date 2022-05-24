101162bd4bdd7cf18bfbe5187ffac31c12c0f30d
MerabUN <merabi.tskhvedadze@gmail.com>
2022-05-24 20:56:01 +0400

    assignment 2

--- src/components/NewExpense/ExpensesFilter.js
+++ src/components/NewExpense/ExpensesFilter.js
@@ -2,17 +2,17 @@ import React from "react";
 
 import "./ExpensesFilter.css";
 
-const ExpensesFilter = (props) => {
+const ExpensesFilter = (props)=> {
 
-  const dropdownChangeHandler = (event) => {
-    props.onChangeFilter(event.target.value);
-  }
+  const changeYear = event => {
+    props.onSaveYear(event.target.value);
+  };
 
   return (
     <div className="expenses-filter">
       <div className="expenses-filter__control">
         <label>Filter by year</label>
-        <select value={props.defaultYear} onChange={dropdownChangeHandler}>
+        <select value={props.defaultYear} onChange={changeYear}>
           <option value="2022">2022</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
